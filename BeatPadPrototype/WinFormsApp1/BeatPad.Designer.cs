963ae7c44a840b42d9a070d3672d83b58bc7b4fd
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-03-28 00:38:01 -0700

    add beatformatter option

--- BeatPadPrototype/WinFormsApp1/BeatPad.Designer.cs
+++ BeatPadPrototype/WinFormsApp1/BeatPad.Designer.cs
@@ -37,7 +37,7 @@
             menuStrip1 = new MenuStrip();
             additionToolStripMenuItem = new ToolStripMenuItem();
             metronomeToolStripMenuItem = new ToolStripMenuItem();
-            menuStrip1.SuspendLayout();
+            beatFormatterToolStripMenuItem = new ToolStripMenuItem();
             button4 = new Button();
             upload4 = new Button();
             upload8 = new Button();
@@ -56,17 +56,15 @@
             button11 = new Button();
             button10 = new Button();
             button9 = new Button();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Enabled = false;
-            button1.Location = new Point(174, 171);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Location = new Point(0, 0);
+            button1.Location = new Point(12, 27);
             button1.Name = "button1";
-            button1.Size = new Size(94, 91);
-            button1.Size = new Size(135, 115);
+            button1.Size = new Size(118, 86);
             button1.TabIndex = 0;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
@@ -74,12 +72,9 @@
             // button2
             // 
             button2.Enabled = false;
-            button2.Location = new Point(382, 171);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Location = new Point(132, 0);
+            button2.Location = new Point(128, 27);
             button2.Name = "button2";
-            button2.Size = new Size(94, 91);
-            button2.Size = new Size(135, 115);
+            button2.Size = new Size(118, 86);
             button2.TabIndex = 1;
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
@@ -87,12 +82,9 @@
             // button3
             // 
             button3.Enabled = false;
-            button3.Location = new Point(598, 171);
-            button3.Margin = new Padding(3, 4, 3, 4);
-            button3.Location = new Point(264, 0);
+            button3.Location = new Point(243, 27);
             button3.Name = "button3";
-            button3.Size = new Size(94, 91);
-            button3.Size = new Size(135, 115);
+            button3.Size = new Size(118, 86);
             button3.TabIndex = 2;
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
@@ -100,12 +92,9 @@
             // upload1
             // 
             upload1.AccessibleName = "upload1";
-            upload1.Location = new Point(174, 269);
-            upload1.Margin = new Padding(3, 4, 3, 4);
-            upload1.Location = new Point(30, 90);
+            upload1.Location = new Point(38, 95);
             upload1.Name = "upload1";
-            upload1.Size = new Size(94, 31);
-            upload1.Size = new Size(75, 25);
+            upload1.Size = new Size(66, 19);
             upload1.TabIndex = 3;
             upload1.Text = "upload";
             upload1.UseVisualStyleBackColor = true;
@@ -114,12 +103,9 @@
             // upload2
             // 
             upload2.AccessibleName = "upload2";
-            upload2.Location = new Point(382, 269);
-            upload2.Margin = new Padding(3, 4, 3, 4);
-            upload2.Location = new Point(161, 90);
+            upload2.Location = new Point(153, 95);
             upload2.Name = "upload2";
-            upload2.Size = new Size(94, 31);
-            upload2.Size = new Size(75, 25);
+            upload2.Size = new Size(66, 19);
             upload2.TabIndex = 4;
             upload2.Text = "upload";
             upload2.UseVisualStyleBackColor = true;
@@ -128,12 +114,9 @@
             // upload3
             // 
             upload3.AccessibleName = "upload3";
-            upload3.Location = new Point(598, 269);
-            upload3.Margin = new Padding(3, 4, 3, 4);
-            upload3.Location = new Point(290, 90);
+            upload3.Location = new Point(266, 95);
             upload3.Name = "upload3";
-            upload3.Size = new Size(94, 31);
-            upload3.Size = new Size(75, 25);
+            upload3.Size = new Size(66, 19);
             upload3.TabIndex = 5;
             upload3.Text = "upload";
             upload3.UseVisualStyleBackColor = true;
@@ -145,39 +128,49 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { additionToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(901, 28);
+            menuStrip1.Padding = new Padding(5, 2, 0, 2);
+            menuStrip1.Size = new Size(486, 24);
             menuStrip1.TabIndex = 6;
             menuStrip1.Text = "menuStrip1";
             // 
             // additionToolStripMenuItem
             // 
-            additionToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { metronomeToolStripMenuItem });
+            additionToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { metronomeToolStripMenuItem, beatFormatterToolStripMenuItem });
             additionToolStripMenuItem.Name = "additionToolStripMenuItem";
-            additionToolStripMenuItem.Size = new Size(81, 24);
+            additionToolStripMenuItem.Size = new Size(65, 20);
             additionToolStripMenuItem.Text = "Addition";
             // 
             // metronomeToolStripMenuItem
             // 
             metronomeToolStripMenuItem.Name = "metronomeToolStripMenuItem";
-            metronomeToolStripMenuItem.Size = new Size(224, 26);
+            metronomeToolStripMenuItem.Size = new Size(180, 22);
             metronomeToolStripMenuItem.Text = "Metronome";
             metronomeToolStripMenuItem.Click += metronomeToolStripMenuItem_Click;
+            // 
+            // beatFormatterToolStripMenuItem
+            // 
+            beatFormatterToolStripMenuItem.Name = "beatFormatterToolStripMenuItem";
+            beatFormatterToolStripMenuItem.Size = new Size(180, 22);
+            beatFormatterToolStripMenuItem.Text = "Beat Formatter";
+            beatFormatterToolStripMenuItem.Click += beatFormatterToolStripMenuItem_Click;
             // 
             // button4
             // 
             button4.Enabled = false;
-            button4.Location = new Point(396, 0);
+            button4.Location = new Point(358, 27);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(135, 115);
+            button4.Size = new Size(118, 86);
             button4.TabIndex = 6;
             button4.UseVisualStyleBackColor = true;
             // 
             // upload4
             // 
             upload4.AccessibleName = "upload4";
-            upload4.Location = new Point(425, 90);
+            upload4.Location = new Point(384, 95);
+            upload4.Margin = new Padding(3, 2, 3, 2);
             upload4.Name = "upload4";
-            upload4.Size = new Size(75, 25);
+            upload4.Size = new Size(66, 19);
             upload4.TabIndex = 7;
             upload4.Text = "upload";
             upload4.UseVisualStyleBackColor = true;
@@ -186,9 +179,10 @@
             // upload8
             // 
             upload8.AccessibleName = "upload8";
-            upload8.Location = new Point(425, 204);
+            upload8.Location = new Point(384, 180);
+            upload8.Margin = new Padding(3, 2, 3, 2);
             upload8.Name = "upload8";
-            upload8.Size = new Size(75, 25);
+            upload8.Size = new Size(66, 19);
             upload8.TabIndex = 15;
             upload8.Text = "upload";
             upload8.UseVisualStyleBackColor = true;
@@ -197,9 +191,10 @@
             // button8
             // 
             button8.Enabled = false;
-            button8.Location = new Point(396, 114);
+            button8.Location = new Point(358, 113);
+            button8.Margin = new Padding(3, 2, 3, 2);
             button8.Name = "button8";
-            button8.Size = new Size(135, 115);
+            button8.Size = new Size(118, 86);
             button8.TabIndex = 14;
             button8.UseVisualStyleBackColor = true;
             button8.Click += button8_Click;
@@ -207,9 +202,10 @@
             // upload7
             // 
             upload7.AccessibleName = "upload7";
-            upload7.Location = new Point(290, 204);
+            upload7.Location = new Point(266, 180);
+            upload7.Margin = new Padding(3, 2, 3, 2);
             upload7.Name = "upload7";
-            upload7.Size = new Size(75, 25);
+            upload7.Size = new Size(66, 19);
             upload7.TabIndex = 13;
             upload7.Text = "upload";
             upload7.UseVisualStyleBackColor = true;
@@ -218,9 +214,10 @@
             // upload6
             // 
             upload6.AccessibleName = "upload6";
-            upload6.Location = new Point(161, 204);
+            upload6.Location = new Point(153, 180);
+            upload6.Margin = new Padding(3, 2, 3, 2);
             upload6.Name = "upload6";
-            upload6.Size = new Size(75, 25);
+            upload6.Size = new Size(66, 19);
             upload6.TabIndex = 12;
             upload6.Text = "upload";
             upload6.UseVisualStyleBackColor = true;
@@ -229,9 +226,10 @@
             // upload5
             // 
             upload5.AccessibleName = "upload5";
-            upload5.Location = new Point(30, 204);
+            upload5.Location = new Point(38, 180);
+            upload5.Margin = new Padding(3, 2, 3, 2);
             upload5.Name = "upload5";
-            upload5.Size = new Size(75, 25);
+            upload5.Size = new Size(66, 19);
             upload5.TabIndex = 11;
             upload5.Text = "upload";
             upload5.UseVisualStyleBackColor = true;
@@ -240,9 +238,10 @@
             // button7
             // 
             button7.Enabled = false;
-            button7.Location = new Point(264, 114);
+            button7.Location = new Point(243, 113);
+            button7.Margin = new Padding(3, 2, 3, 2);
             button7.Name = "button7";
-            button7.Size = new Size(135, 115);
+            button7.Size = new Size(118, 86);
             button7.TabIndex = 10;
             button7.UseVisualStyleBackColor = true;
             button7.Click += button7_Click;
@@ -250,9 +249,10 @@
             // button6
             // 
             button6.Enabled = false;
-            button6.Location = new Point(132, 114);
+            button6.Location = new Point(128, 113);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(135, 115);
+            button6.Size = new Size(118, 86);
             button6.TabIndex = 9;
             button6.UseVisualStyleBackColor = true;
             button6.Click += button6_Click;
@@ -260,9 +260,10 @@
             // button5
             // 
             button5.Enabled = false;
-            button5.Location = new Point(0, 114);
+            button5.Location = new Point(12, 113);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(135, 115);
+            button5.Size = new Size(118, 86);
             button5.TabIndex = 8;
             button5.UseVisualStyleBackColor = true;
             button5.Click += button5_Click;
@@ -270,9 +271,10 @@
             // upload12
             // 
             upload12.AccessibleName = "upload12";
-            upload12.Location = new Point(425, 315);
+            upload12.Location = new Point(384, 263);
+            upload12.Margin = new Padding(3, 2, 3, 2);
             upload12.Name = "upload12";
-            upload12.Size = new Size(75, 25);
+            upload12.Size = new Size(66, 19);
             upload12.TabIndex = 23;
             upload12.Text = "upload";
             upload12.UseVisualStyleBackColor = true;
@@ -282,9 +284,10 @@
             // 
             button12.AccessibleName = "upload12";
             button12.Enabled = false;
-            button12.Location = new Point(396, 225);
+            button12.Location = new Point(358, 196);
+            button12.Margin = new Padding(3, 2, 3, 2);
             button12.Name = "button12";
-            button12.Size = new Size(135, 115);
+            button12.Size = new Size(118, 86);
             button12.TabIndex = 22;
             button12.UseVisualStyleBackColor = true;
             button12.Click += button12_Click;
@@ -292,9 +295,10 @@
             // upload11
             // 
             upload11.AccessibleName = "upload11";
-            upload11.Location = new Point(290, 315);
+            upload11.Location = new Point(266, 263);
+            upload11.Margin = new Padding(3, 2, 3, 2);
             upload11.Name = "upload11";
-            upload11.Size = new Size(75, 25);
+            upload11.Size = new Size(66, 19);
             upload11.TabIndex = 21;
             upload11.Text = "upload";
             upload11.UseVisualStyleBackColor = true;
@@ -303,9 +307,10 @@
             // upload10
             // 
             upload10.AccessibleName = "upload10";
-            upload10.Location = new Point(161, 315);
+            upload10.Location = new Point(153, 263);
+            upload10.Margin = new Padding(3, 2, 3, 2);
             upload10.Name = "upload10";
-            upload10.Size = new Size(75, 25);
+            upload10.Size = new Size(66, 19);
             upload10.TabIndex = 20;
             upload10.Text = "upload";
             upload10.UseVisualStyleBackColor = true;
@@ -314,9 +319,10 @@
             // upload9
             // 
             upload9.AccessibleName = "upload9";
-            upload9.Location = new Point(30, 315);
+            upload9.Location = new Point(38, 263);
+            upload9.Margin = new Padding(3, 2, 3, 2);
             upload9.Name = "upload9";
-            upload9.Size = new Size(75, 25);
+            upload9.Size = new Size(66, 19);
             upload9.TabIndex = 19;
             upload9.Text = "upload";
             upload9.UseVisualStyleBackColor = true;
@@ -325,9 +331,10 @@
             // button11
             // 
             button11.Enabled = false;
-            button11.Location = new Point(264, 225);
+            button11.Location = new Point(243, 196);
+            button11.Margin = new Padding(3, 2, 3, 2);
             button11.Name = "button11";
-            button11.Size = new Size(135, 115);
+            button11.Size = new Size(118, 86);
             button11.TabIndex = 18;
             button11.UseVisualStyleBackColor = true;
             button11.Click += button11_Click;
@@ -335,9 +342,10 @@
             // button10
             // 
             button10.Enabled = false;
-            button10.Location = new Point(132, 225);
+            button10.Location = new Point(128, 196);
+            button10.Margin = new Padding(3, 2, 3, 2);
             button10.Name = "button10";
-            button10.Size = new Size(135, 115);
+            button10.Size = new Size(118, 86);
             button10.TabIndex = 17;
             button10.UseVisualStyleBackColor = true;
             button10.Click += button10_Click;
@@ -345,19 +353,19 @@
             // button9
             // 
             button9.Enabled = false;
-            button9.Location = new Point(0, 225);
+            button9.Location = new Point(12, 196);
+            button9.Margin = new Padding(3, 2, 3, 2);
             button9.Name = "button9";
-            button9.Size = new Size(135, 115);
+            button9.Size = new Size(118, 86);
             button9.TabIndex = 16;
             button9.UseVisualStyleBackColor = true;
             button9.Click += button9_Click;
             // 
             // BeatPad
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(901, 697);
-            ClientSize = new Size(530, 339);
+            ClientSize = new Size(486, 295);
             Controls.Add(upload12);
             Controls.Add(button12);
             Controls.Add(upload11);
@@ -384,13 +392,11 @@
             Controls.Add(button1);
             Controls.Add(menuStrip1);
             MainMenuStrip = menuStrip1;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "BeatPad";
-            Text = "Form1";
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
             Text = "BeatPad";
             Load += BeatPad_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -423,5 +429,6 @@
         private Button button11;
         private Button button10;
         private Button button9;
+        private ToolStripMenuItem beatFormatterToolStripMenuItem;
     }
 }
