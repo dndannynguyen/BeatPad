1f04fda035908ca0a1d0cd5e744d9c5aff778c77
dndannynguyen <dn.dannynguyen@hotmail.com>
2024-04-11 01:34:10 -0700

    Update the sounds formation that it is able to play multiple sounds at a time in one cycle

--- BeatPadPrototype/WinFormsApp1/FourBar.cs
+++ BeatPadPrototype/WinFormsApp1/FourBar.cs
@@ -8,23 +8,16 @@ namespace WinFormsApp1
     /// <summary>
     /// Danny Nguyen
     /// 
-    /// Represents the main form of the application for playing sound files in a loop.
+    /// Represents the main form of the application for playing sound files in a loop of the four bar beat formation.
     /// </summary>
     public partial class FourBar : Form
     {
-        // Instance variables
         BeatSoundUpload uploadForm = new BeatSoundUpload();
         AudioPlayer player;
         string[] buttonFilepaths = new string[32];
-        string buttonSound1Filepath;
-        string buttonSound2Filepath;
-        string buttonSound3Filepath;
-        string buttonSound4Filepath;
+        string[] buttonSoundFilepaths = new string[4];
         int currentButtonIndex = 0;
-        HashSet<int> sound1ClickedButtonIndexes = new HashSet<int>();
-        HashSet<int> sound2ClickedButtonIndexes = new HashSet<int>();
-        HashSet<int> sound3ClickedButtonIndexes = new HashSet<int>();
-        HashSet<int> sound4ClickedButtonIndexes = new HashSet<int>();
+        HashSet<int>[] soundClickedButtonIndexes = new HashSet<int>[4]; // Array to store clicked button indexes for each sound button
         int soundButtonAt = 1;
 
 
@@ -36,6 +29,12 @@ namespace WinFormsApp1
             InitializeComponent();
             this.player = new AudioPlayer();
             InitializeButtons();
+
+            for (int i = 0; i < 4; i++)
+            {
+                soundClickedButtonIndexes[i] = new HashSet<int>();
+            }
+
             PlayButtonsLoop();
 
         }
@@ -62,39 +61,22 @@ namespace WinFormsApp1
             {
                 for (int i = 0; i < 32; i++)
                 {
-                    if (sound1ClickedButtonIndexes.Contains(i) || sound2ClickedButtonIndexes.Contains(i)  || sound3ClickedButtonIndexes.Contains(i) || sound4ClickedButtonIndexes.Contains(i))
+                    if (soundClickedButtonIndexes[0].Contains(i) || soundClickedButtonIndexes[1].Contains(i) || soundClickedButtonIndexes[2].Contains(i) || soundClickedButtonIndexes[3].Contains(i))
                     {
-                        if (sound1ClickedButtonIndexes.Contains(i))
+                        for (int j = 0; j < 4; j++)
                         {
-                            // Play the sound asynchronously
-                            await Task.Run(() => player.PlayAudio(buttonSound1Filepath));
-                            currentButtonIndex = (currentButtonIndex + 1) % 32;
-                            await Task.Delay(250);
-                        }
-                        if (sound2ClickedButtonIndexes.Contains(i))
-                        {
-                            // Play the sound asynchronously
-                            await Task.Run(() => player.PlayAudio(buttonSound2Filepath));
-                            currentButtonIndex = (currentButtonIndex + 1) % 32;
-                            await Task.Delay(250);
-                        }
-                        if (sound3ClickedButtonIndexes.Contains(i))
-                        {
-                            // Play the sound asynchronously
-                            await Task.Run(() => player.PlayAudio(buttonSound3Filepath));
-                            currentButtonIndex = (currentButtonIndex + 1) % 32;
-                            await Task.Delay(250);
-                        }
-                        if (sound4ClickedButtonIndexes.Contains(i))
-                        {
-                            // Play the sound asynchronously
-                            await Task.Run(() => player.PlayAudio(buttonSound4Filepath));
-                            currentButtonIndex = (currentButtonIndex + 1) % 32;
-                            await Task.Delay(250);
+                            if (soundClickedButtonIndexes[j].Contains(i))
+                            {
+                                // Play the sound asynchronously
+                                await Task.Run(() => player.PlayAudio(buttonSoundFilepaths[j]));
+                                await Task.Delay(250);
+                                currentButtonIndex = (currentButtonIndex + 1) % 32;
+                            }
                         }
                     }
                     else
                     {
+                        // If the button is not set to play sounds in the period, keep the green background going through.
                         Controls[$"button{i + 1}"].BackColor = Color.Green;
                         await Task.Delay(250);
                         Controls[$"button{i + 1}"].BackColor = Color.White;
@@ -109,165 +91,68 @@ namespace WinFormsApp1
         /// </summary>
         private void Button_Click(object sender, EventArgs e)
         {
-            // Get the index of the clicked button
             Button clickedButton = sender as Button;
-            int clickedIndex = int.Parse(clickedButton.Name.Substring(6)) - 1; // Extract the button index from its name
+            int clickedIndex = int.Parse(clickedButton.Name.Substring(6)) - 1;
 
-            // Toggle button selection
-            if (sound1ClickedButtonIndexes.Contains(clickedIndex))
-            {
-                // Deselect the button
-                sound1ClickedButtonIndexes.Remove(clickedIndex);
-                clickedButton.BackColor = Color.White;
-            }
-            else
-            {
-                // Select the button
-                sound1ClickedButtonIndexes.Add(clickedIndex);
-                clickedButton.BackColor = Color.Red;
-            }
+            // Select the sound button
+            soundClickedButtonIndexes[soundButtonAt - 1].SymmetricExceptWith(new HashSet<int> { clickedIndex });
+            clickedButton.BackColor = soundClickedButtonIndexes[soundButtonAt - 1].Contains(clickedIndex) ? Color.Pink : Color.White;
         }
 
         /// <summary>
         /// Event handler for uploading sound files. Assigns the uploaded file path to the current button.
         /// </summary>
-        private void buttonUpload1_Click(object sender, EventArgs e)
+        private void buttonUpload_Click(object sender, EventArgs e)
         {
-            // Reset upload form
+            Button uploadButton = sender as Button;
+            int soundButtonIndex = int.Parse(uploadButton.Name.Substring(12, 1));
+
             uploadForm.resetForm();
-            // Show the upload form
             uploadForm.ShowDialog();
 
-            // If user successfully uploads file
             if (uploadForm.filepath != null)
             {
-                // Assign the uploaded file path to buttonSound1
-                buttonSound1Filepath = uploadForm.filepath;
-                // Enable the current button
+                buttonSoundFilepaths[soundButtonIndex - 1] = uploadForm.filepath;
                 Controls[$"button{currentButtonIndex + 1}"].Enabled = true;
-                // Change the button name if user added a name
                 if (uploadForm.name != null)
                 {
-                    Controls[$"buttonSound1"].Text = uploadForm.name;
+                    Controls[$"buttonSound{soundButtonIndex}"].Text = uploadForm.name;
                 }
             }
         }
 
         /// <summary>
-        /// Event handler for uploading sound files. Assigns the uploaded file path to the current button.
+        /// Event handler for when a sound button is clicked. Sets the currently selected sound button and refreshes the UI accordingly.
         /// </summary>
-        private void buttonUpload2_Click(object sender, EventArgs e)
+        private void buttonSound_Click(object sender, EventArgs e)
         {
-            // Reset upload form
-            uploadForm.resetForm();
-            // Show the upload form
-            uploadForm.ShowDialog();
+            Button soundButton = sender as Button;
+            soundButtonAt = int.Parse(soundButton.Name.Substring(11, 1));
+            RefreshUI();
+            ShowUI();
+        }
 
-            // If user successfully uploads file
-            if (uploadForm.filepath != null)
+        /// <summary>
+        /// Shows the UI configuration for the currently selected sound button, highlighting the buttons that are selected.
+        /// </summary>
+        private void ShowUI()
+        {
+            for (int i = 0; i < 32; i++)
             {
-                // Assign the uploaded file path to buttonSound2
-                buttonSound2Filepath = uploadForm.filepath;
-                // Enable the current button
-                Controls[$"button{currentButtonIndex + 1}"].Enabled = true;
-                // Change the button name if user added a name
-                if (uploadForm.name != null)
-                {
-                    Controls[$"buttonSound2"].Text = uploadForm.name;
-                }
+                Button button = Controls[$"button{i + 1}"] as Button;
+                button.BackColor = soundClickedButtonIndexes[soundButtonAt - 1].Contains(i) ? Color.Pink : Color.White;
             }
         }
 
         /// <summary>
-        /// Event handler for uploading sound files. Assigns the uploaded file path to the current button.
+        /// Refreshes the UI for all 32 buttons, resetting their colors to default.
         /// </summary>
-        private void buttonUpload3_Click(object sender, EventArgs e)
+        private void RefreshUI()
         {
-            // Reset upload form
-            uploadForm.resetForm();
-            // Show the upload form
-            uploadForm.ShowDialog();
-
-            // If user successfully uploads file
-            if (uploadForm.filepath != null)
+            for (int i = 0; i < 32; i++)
             {
-                // Assign the uploaded file path to buttonSound3
-                buttonSound3Filepath = uploadForm.filepath;
-                // Enable the current button
-                Controls[$"button{currentButtonIndex + 1}"].Enabled = true;
-                // Change the button name if user added a name
-                if (uploadForm.name != null)
-                {
-                    Controls[$"buttonSound3"].Text = uploadForm.name;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Event handler for uploading sound files. Assigns the uploaded file path to the current button.
-        /// </summary>
-        private void buttonUpload4_Click(object sender, EventArgs e)
-        {
-            // Reset upload form
-            uploadForm.resetForm();
-            // Show the upload form
-            uploadForm.ShowDialog();
-
-            // If user successfully uploads file
-            if (uploadForm.filepath != null)
-            {
-                // Assign the uploaded file path to buttonSound4
-                buttonSound4Filepath = uploadForm.filepath;
-                // Enable the current button
-                Controls[$"button{currentButtonIndex + 1}"].Enabled = true;
-                // Change the button name if user added a name
-                if (uploadForm.name != null)
-                {
-                    Controls[$"buttonSound4"].Text = uploadForm.name;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Event handler for assigning a sound file to buttonSound1 and starting the loop.
-        /// </summary>
-        private async void buttonSound1_Click(object sender, EventArgs e)
-        {
-            soundButtonAt = 1;
-            buttonSound1Filepath = uploadForm.filepath;
-            if (!string.IsNullOrEmpty(buttonSound1Filepath))
-            {
-                await Task.Run(() => player.PlayAudio(buttonSound1Filepath)); // Play the sound once
-            }
-        }
-
-        private async void buttonSound2_Click(object sender, EventArgs e)
-        {
-            soundButtonAt = 2;
-            buttonSound2Filepath = uploadForm.filepath;
-            if (!string.IsNullOrEmpty(buttonSound2Filepath))
-            {
-                await Task.Run(() => player.PlayAudio(buttonSound2Filepath)); // Play the sound once
-            }
-        }
-
-        private async void buttonSound3_Click(object sender, EventArgs e)
-        {
-            soundButtonAt = 3;
-            buttonSound3Filepath = uploadForm.filepath;
-            if (!string.IsNullOrEmpty(buttonSound3Filepath))
-            {
-                await Task.Run(() => player.PlayAudio(buttonSound3Filepath)); // Play the sound once
-            }
-        }
-
-        private async void buttonSound4_Click(object sender, EventArgs e)
-        {
-            soundButtonAt = 4;
-            buttonSound4Filepath = uploadForm.filepath;
-            if (!string.IsNullOrEmpty(buttonSound4Filepath))
-            {
-                await Task.Run(() => player.PlayAudio(buttonSound4Filepath)); // Play the sound once
+                Button button = Controls[$"button{i + 1}"] as Button;
+                button.BackColor = Color.White;
             }
         }
     }
