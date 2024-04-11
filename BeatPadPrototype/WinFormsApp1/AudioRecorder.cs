1cc40e81845a952dd8d7cc3140cf164fbf1093fa
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2024-04-10 22:18:31 -0700

    implemented audio recorder

---
+++ BeatPadPrototype/WinFormsApp1/AudioRecorder.cs
@@ -0,0 +1,62 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    /// <summary>
+    /// SeungJae Baek
+    /// 
+    /// Audio Recorder
+    /// </summary>
+    internal class AudioRecorder
+    {
+        private WasapiLoopbackCapture? capture;
+        private WaveFileWriter? waveFileWriter;
+
+        public Array LoadDevices()
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDeviceCollection devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+            return devices.ToArray();
+        }
+
+        public void StartRecording(string fileName, MMDevice device)
+        {
+            this.capture = new WasapiLoopbackCapture(device);
+            this.waveFileWriter = new WaveFileWriter(fileName, capture.WaveFormat);
+            capture.DataAvailable += (_, @event) =>
+            {
+                waveFileWriter.Write(@event.Buffer, 0, @event.BytesRecorded);
+            };
+
+            capture.RecordingStopped += (_, @event) =>
+            {
+                waveFileWriter.Dispose();
+                capture.Dispose();
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = Path.GetDirectoryName(fileName),
+                    UseShellExecute = true,
+                };
+                Process.Start(startInfo);
+            };
+
+            capture.StartRecording();
+        }
+
+        public void StopRecording()
+        {
+            if (capture != null)
+            {
+                capture.StopRecording();
+            }
+        }
+    }
+}
